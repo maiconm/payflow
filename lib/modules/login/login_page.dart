9b6cc5ca2490e0054a6e6bb2a17e49a2dc129664
maiconm <maiconm23@gmail.com>
2021-06-23 19:31:36 -0300

    Cria LoginController

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -14,6 +15,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  final controller = LoginController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -67,19 +71,9 @@ class _LoginPageState extends State<LoginPage> {
                       right: 40,
                       top: 40
                     ),
-                    child: SocialLoginButton(onTap: () async {
-                      GoogleSignIn _googleSignIn = GoogleSignIn(
-                        scopes: [
-                          'email',
-                        ],
-                      );
-                      try {
-                        final response = await _googleSignIn.signIn();
-                        print(response);
-                      } catch (error) {
-                        print(error);
-                      }
-                    }),
+                    child: SocialLoginButton(
+                      onTap: controller.googleSignIn
+                    ),
                   ),
                 ],
               ),
