9f4808d201870ade302b5ccaa8984229e233380b
maiconm <maiconm23@gmail.com>
2021-06-25 22:03:21 -0300

    Configura layout da tela de boleto e insere cor input

--- lib/modules/insert_billet/insert_billet_page.dart
+++ lib/modules/insert_billet/insert_billet_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class InsertBilletPage extends StatelessWidget {
   const InsertBilletPage({ Key? key }) : super(key: key);
@@ -7,7 +9,24 @@ class InsertBilletPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Insert Boleto Page"),
+        backgroundColor: AppColors.background,
+        elevation: 0,
+        leading: BackButton(color: AppColors.input,),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 93,
+              vertical: 24
+            ),
+            child: Text(
+              "Preencha os dados do boleto",
+              style: TextStyles.titleBoldHeading,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
       ),
     );
   }
