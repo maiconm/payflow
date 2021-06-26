cbc69717e91d1ad3976c41af534da06990af2a84
maiconm <maiconm23@gmail.com>
2021-06-25 23:04:36 -0300

    Corrige bug de overflow de layout e adiciona os métodos onChange em cada input

--- lib/modules/insert_billet/insert_billet_page.dart
+++ lib/modules/insert_billet/insert_billet_page.dart
@@ -55,57 +55,67 @@ class _InsertBilletPageState extends State<InsertBilletPage> {
         elevation: 0,
         leading: BackButton(color: AppColors.input,),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 70,
-                vertical: 24
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 70,
+                  vertical: 24
+                ),
+                child: Text(
+                  "Preencha os dados do boleto",
+                  style: TextStyles.titleBoldHeading,
+                  textAlign: TextAlign.center,
+                ),
               ),
-              child: Text(
-                "Preencha os dados do boleto",
-                style: TextStyles.titleBoldHeading,
-                textAlign: TextAlign.center,
+              SizedBox(height: 24,),
+              Form(
+                key: controller.formKey,
+                child: Column(
+                  children: [
+                    InputText(
+                      label: "Nome do boleto",
+                      icon: Icons.description_outlined,
+                      validator: controller.validateName,
+                      onChanged: (value) {
+                        controller.onChange(name: value);
+                      },
+                    ),
+                    InputText(
+                      label: "Vemcimento",
+                      controller: dueDateInputTextController,
+                      icon: FontAwesomeIcons.timesCircle,
+                      validator: controller.validateDueDate,
+                      onChanged: (value) {
+                        controller.onChange(dueDate: value);
+                      },
+                    ),
+                    InputText(
+                      label: "Valor",
+                      controller: moneyInputTextController,
+                      icon: FontAwesomeIcons.wallet,
+                      validator: (_) => controller.validateValue(moneyInputTextController.numberValue),
+                      onChanged: (value) {
+                        controller.onChange(value: moneyInputTextController.numberValue);
+                      },
+                    ),
+                    InputText(
+                      label: "Código",
+                      controller: barcodeInputTextController,
+                      icon: FontAwesomeIcons.barcode,
+                      validator: controller.validateBarcode,
+                      onChanged: (value) {
+                        controller.onChange(bardode: value);
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 24,),
-            Form(
-              key: controller.formKey,
-              child: Column(
-                children: [
-                  InputText(
-                    label: "Nome do boleto",
-                    icon: Icons.description_outlined,
-                    validator: controller.validateName,
-                    onChanged: (value) {},
-                  ),
-                  InputText(
-                    label: "Vemcimento",
-                    controller: dueDateInputTextController,
-                    icon: FontAwesomeIcons.timesCircle,
-                    validator: controller.validateDueDate,
-                    onChanged: (value) {},
-                  ),
-                  InputText(
-                    label: "Valor",
-                    controller: moneyInputTextController,
-                    icon: FontAwesomeIcons.wallet,
-                    validator: (_) => controller.validateValue(moneyInputTextController.numberValue),
-                    onChanged: (value) {},
-                  ),
-                  InputText(
-                    label: "Código",
-                    controller: barcodeInputTextController,
-                    icon: FontAwesomeIcons.barcode,
-                    validator: controller.validateBarcode,
-                    onChanged: (value) {},
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: SetLabelButtons(
@@ -114,7 +124,9 @@ class _InsertBilletPageState extends State<InsertBilletPage> {
           Navigator.pop(context);
         },
         secondaryLabel: "Cadastrar",
-        secondaryOnPressed: (){},
+        secondaryOnPressed: (){
+          controller.registerBillet();
+        },
         enableSecondaryColor: true,
       ),
     );
