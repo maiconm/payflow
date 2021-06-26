05d8b1ff83c6b373a84e477c857094aabb143403
maiconm <maiconm23@gmail.com>
2021-06-25 22:46:12 -0300

    Cria controller com validações de inputs da tela de cadastro de boletos

---
+++ lib/modules/insert_billet/insert_billet_controller.dart
@@ -0,0 +1,11 @@
+class InsertBilletController {
+
+  String? validateName(String? value) => value?.isEmpty ?? true ? "O nome não pode ser vazio" : null;
+
+  String? validateDueDate(String? value) => value?.isEmpty ?? true ? "A data de vencimento não pode ser vazio" : null;
+  
+  String? validateValue(double value) => value == 0 ? "Insira um valor maior que R\$ 0,00" : null;
+
+  String? validateBarcode(String? value) => value?.isEmpty ?? true ? "O código do boleto não pode ser vazio" : null;
+
+}
